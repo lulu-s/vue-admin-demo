3d1bb5b761d4e71ca25772377aff90b4d9f0d770
lulu-s <1192707005@qq.com>
2020-04-17 12:35:51 +0800

    add excel

--- src/config/global.config.js
+++ src/config/global.config.js
@@ -12,6 +12,28 @@ const data = {
         username: "admin",
         password: "123456"
     },
+    tableData: [
+        {
+            date: '2016-05-02',
+            name: '王小虎0',
+            address: '上海市普陀区金沙江路 1518 弄'
+        }, 
+        {
+            date: '2016-05-04',
+            name: '王小虎1',
+            address: '上海市普陀区金沙江路 1517 弄'
+        }, 
+        {
+            date: '2016-05-01',
+            name: '王小虎2',
+            address: '上海市普陀区金沙江路 1519 弄'
+        }, 
+        {
+            date: '2016-05-03',
+            name: '王小虎3',
+            address: '上海市普陀区金沙江路 1516 弄'
+        }
+    ],
     menu: [
         {
             name: "仪表盘",
--- src/views/ManageIp.vue
+++ src/views/ManageIp.vue
@@ -1,58 +1,47 @@
 <template>
-  <el-table
-    :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
-    style="width: 100%">
-    <el-table-column
-      label="Date"
-      prop="date">
-    </el-table-column>
-    <el-table-column
-      label="Name"
-      prop="name">
-    </el-table-column>
-    <el-table-column
-      align="right">
-      <template slot="header" slot-scope="scope">
-        <el-input
-          v-model="search"
-          size="mini"
-          placeholder="输入关键字搜索"/>
-      </template>
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <!-- v-waves -->
+    <div class="filter-container">
+      <el-input style="width: 200px;" v-model="search" placeholder="输入关键字搜索"></el-input>
+      <el-button :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">
+        Export
+      </el-button>
+    </div>
+    <el-table
+      :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+      style="width: 100%">
+      <el-table-column
+        label="Date"
+        prop="date">
+      </el-table-column>
+      <el-table-column
+        label="Name"
+        prop="name">
+      </el-table-column>
+      <el-table-column label="Action" >
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
+  import {tableData} from "../config/global.config"
+  import {parseTime} from "../utils"
   export default {
     data() {
       return {
-        tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }],
-        search: ''
+        tableData,
+        search: '',
+        downloadLoading: false
       }
     },
     methods: {
@@ -60,8 +49,47 @@
         console.log(index, row);
       },
       handleDelete(index, row) {
-        console.log(index, row);
-      }
+        // https://element.eleme.cn/#/zh-CN/component/notification
+        this.$notify({
+          title: 'Success',
+          message: 'Delete Successfully',
+          type: 'success',
+          duration: 2000
+        })
+        this.tableData.splice(index, 1)
+      },
+      handleDownload() {
+        this.downloadLoading = true
+        import('../utils/Export2Excel').then(excel => {
+          const tHeader = ['date', 'name']
+          const filterVal = ['date', 'name']
+          const data = this.formatJson(filterVal)
+          excel.export_json_to_excel({
+            header: tHeader,
+            data,
+            filename: 'table-list'
+          })
+          this.downloadLoading = false
+        })
+      },
+      formatJson(filterVal) {
+        return this.tableData.map(v => filterVal.map(j => {
+          if (j === 'Date') {
+            return parseTime(v[j])
+          } else {
+            return v[j]
+          }
+        }))
+      },
     },
   }
 </script>
+
+<style scoped>
+  .filter-container {
+    margin: 10px 0;
+  }
+  .filter-container > * {
+    margin-left: 10px;
+  }
+</style>
